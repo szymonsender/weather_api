2b063a53cb39dee64a60eb2810b6d4f6025f1128
Szymon Sender <senderszymom@gmail.com>
2022-07-26 16:53:15 +0200

    Create HomePage

--- lib/features/home/pages/home_page.dart
+++ lib/features/home/pages/home_page.dart
@@ -1,21 +1,132 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:weather_api/app/enums.dart';
+import 'package:weather_api/data/weather_data_source.dart';
+import 'package:weather_api/features/home/cubit/home_cubit.dart';
+import 'package:weather_api/models/weather_model.dart';
+import 'package:weather_api/repositories/weather_repository.dart';
 
-class HomePage extends StatefulWidget {
+class HomePage extends StatelessWidget {
   const HomePage({
     Key? key,
   }) : super(key: key);
 
   @override
-  State<HomePage> createState() => _HomePageState();
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (context) => HomeCubit(
+        WeatherRepository(WeatherRemoteDataSource()),
+      ),
+      child: BlocConsumer<HomeCubit, HomeState>(
+        listener: (context, state) {
+          if (state.status == Status.error) {
+            final errorMessage = state.errorMessage ?? 'Unkown error';
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(
+                content: Text(errorMessage),
+                backgroundColor: Colors.red,
+              ),
+            );
+          }
+        },
+        builder: (context, state) {
+          final weatherModel = state.model;
+          return Scaffold(
+            appBar: AppBar(
+              title: const Text('Pogodynka'),
+              centerTitle: true,
+            ),
+            body: Center(
+              child: Builder(builder: (context) {
+                if (state.status == Status.loading) {
+                  return const Text('Ładowanie');
+                }
+                return Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    if (weatherModel != null)
+                      _DisplayWeatherWidget(
+                        weatherModel: weatherModel,
+                      ),
+                    _SearchWidget(),
+                  ],
+                );
+              }),
+            ),
+          );
+        },
+      ),
+    );
+  }
 }
 
-class _HomePageState extends State<HomePage> {
+class _DisplayWeatherWidget extends StatelessWidget {
+  const _DisplayWeatherWidget({
+    Key? key,
+    required this.weatherModel,
+  }) : super(key: key);
+
+  final WeatherModel weatherModel;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Pogoda'),
-        centerTitle: true,
+    return BlocBuilder<HomeCubit, HomeState>(
+      builder: (context, state) {
+        return Column(
+          children: [
+            Text(
+              weatherModel.temperature.toString(),
+              style: Theme.of(context).textTheme.headline1,
+            ),
+            const SizedBox(height: 40),
+            Text(
+              weatherModel.city,
+              style: Theme.of(context).textTheme.headline3,
+              textAlign: TextAlign.center,
+            ),
+            Text(
+              weatherModel.time.toString(),
+              style: Theme.of(context).textTheme.headline6,
+            ),
+            const SizedBox(height: 15),
+          ],
+        );
+      },
+    );
+  }
+}
+
+class _SearchWidget extends StatelessWidget {
+  _SearchWidget({
+    Key? key,
+  }) : super(key: key);
+
+  final _controller = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 15.0),
+      child: Row(
+        children: [
+          Expanded(
+            child: TextField(
+              controller: _controller,
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                label: Text('Miasto'),
+                hintText: 'Warszawa',
+              ),
+            ),
+          ),
+          const SizedBox(width: 20),
+          ElevatedButton(
+            onPressed: () {
+              context.read<HomeCubit>().getWeatherModel(city: _controller.text);
+            },
+            child: const Text('Pokaż'),
+          ),
+        ],
       ),
     );
   }
